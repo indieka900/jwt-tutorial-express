0a713fdbc490edbff3a4d4f5d1e6970d02ea4b89
Joseph <indiekaj@gmail.com>
2024-04-25 20:15:50 +0300

    jwt tutorial

--- controllers/main.js
+++ controllers/main.js
@@ -1,5 +1,18 @@
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
+
+const CustomAPIError = require('../errors/custom-error')
+
 const login = async (req, res) => {
-    res.send('Fake login register')
+    const {username, password} = req.body
+    if(!username || !password){
+        throw new CustomAPIError('Please provide username and password',400)
+    }
+
+    const id = new Date().getDate()
+
+    const token = jwt.sign({id, username},process.env.JWT_SECRET,{expiresIn:'30d'})
+    res.status(200).json({msg: 'User created', token})
 }
 
 const dashboard = async (req, res) => {
